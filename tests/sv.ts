2b5fff071e0ae3a94b7201feaeee65c9779933df
Manuel Serret <mserret99@gmail.com>
2025-07-05 13:20:27 +0200

    fix formatting

--- tests/sv.ts
+++ tests/sv.ts
@@ -7,7 +7,8 @@ export async function test(options: RunOptions) {
 		repo: 'sveltejs/cli',
 		branch: 'main',
 		build: 'pnpm build',
-		beforeTest: 'pnpm playwright install chromium && pnpx storybook@latest --version', // prefetch the storybook cli to reduce fetching errors in tests
+		beforeTest:
+			'pnpm playwright install chromium && pnpx storybook@latest --version', // prefetch the storybook cli to reduce fetching errors in tests
 		test: 'pnpm test',
 	})
 }

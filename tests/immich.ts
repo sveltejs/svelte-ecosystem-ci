e9a142d52c9d9ac9758db7541a35d9abbb25e8b4
dominikg <dominik.goepel@gmx.de>
2025-08-21 16:02:04 +0200

    feat: immich test

---
+++ tests/immich.ts
@@ -0,0 +1,27 @@
+import { runInRepo } from '../utils.ts'
+import { RunOptions } from '../types.js'
+
+export async function test(options: RunOptions) {
+	await runInRepo({
+		...options,
+		repo: 'immich-app/immich',
+		branch: 'main',
+		overrides: {
+			'@sveltejs/vite-plugin-svelte': true,
+			'@sveltejs/vite-plugin-svelte-inspector': true,
+			'@sveltejs/kit': true,
+		},
+		build: [
+			'pnpm --dir open-api/typescript-sdk build', // required for web build
+			'pnpm --dir web build',
+		],
+		beforeTest: [
+			'pnpm --filter immich-e2e exec playwright install chromium --shell-only',
+			'pnpm --filter immich-e2e exec docker compose build',
+		],
+		test: [
+			//'pnpm --dir web test', // unit & component tests
+			'pnpm --dir e2e test:web', // playwright e2e tests
+		],
+	})
+}
